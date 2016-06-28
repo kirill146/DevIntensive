4720264ebe4bd2c54ac955ec9fbd397675abaa77
kirill146 <kirill-112.1998@maill.ru>
2016-06-28 16:01:50 +0500

    Добавлен NavigationDrawer

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,20 +1,37 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
     private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+    private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+
+        mCallImg = (ImageView) findViewById(R.id.call_img);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mCallImg.setOnClickListener(this);
+
+        if (savedInstanceState == null) {
+//            showSnackBar("First launch");
+        } else {
+//            showSnackBar("Not first launch");
+        }
     }
 
     @Override
@@ -52,4 +69,29 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
         Log.d(TAG, "onRestart");
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.call_img:
+//                showProgress();
+//                runWithDelay();
+                break;
+        }
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+//    private void runWithDelay() {
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                // Run with delay
+//                hideProgress();
+//            }
+//        }, 5000);
+//    }
 }
